1342aaa810fac55431ac170d3d9516ccd53b4c45
scsoares <sarahcsf2@gmail.com>
2024-04-29 20:33:36 +0100

    update

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { TfiClose } from "react-icons/tfi";
+import { useState } from "react";
 
 export const SidebarMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
+      {isOpen && <SidebarMenu />}
       <div
         id="navbar-overlay"
         className="absolute animate-[reveal_0.3s_ease-in-out] bg-black/40 w-screen h-screen top-0 left-0 z-40 lg:hidden"
@@ -11,6 +15,15 @@ export const SidebarMenu = () => {
         id="navbar-mobile"
         className="absolute animate-[slide_0.5s_ease-in-out] w-4/6 h-screen bg-[#EDEDED] z-50 lg:hidden"
       >
+        <div
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          type="button"
+          className="ml-[220px] mt-[20px]"
+        >
+          <TfiClose />
+        </div>
         <div className="flex items-center justify-center my-28">
           <ul className="flex flex-col gap-10 items-center justify-center text-[16px] tracking-widest text-[#181818] lg:hidden">
             <li className="active:underline active:underline-offset-8 active:decoration-wavy">
